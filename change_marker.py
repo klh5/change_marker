5647cb514b2a91d7785a1318d006a9c40554df15
klh5 <klh5@aber.ac.uk>
2018-04-25 16:49:08 +0100

    Added data cube integration

--- change_marker.py
+++ change_marker.py
@@ -3,7 +3,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import keyboard
+import datacube
+import xarray as xr
+from datacube.api import GridWorkflow
+from datacube.storage.masking import mask_invalid_data
+
+sref_products = ['ls5_arcsi_sref_ingested', 'ls7_arcsi_sref_ingested', 'ls8_arcsi_sref_ingested']
 
 class GetChangePoints(object):
 
@@ -44,44 +49,105 @@ class GetChangePoints(object):
 	def get_xlist(self):
 		return self.point_list
 
+def transformToDf(dataset_to_transform):
+
+	"""Transforms xarray Dataset object into a Pandas dataframe"""
+
+	new_df = pd.DataFrame()
+
+	new_df['datetime'] = dataset_to_transform.time.data
+	new_df['datetime'] = new_df['datetime'].apply(lambda x: x.toordinal())
+
+	for name, var in dataset_to_transform.data_vars.items():
+		new_df[name] = np.reshape(var.data, -1)
+        
+    # Points at the edge of the image could return empty arrays (all 0's) - this will remove any columns to which this applies
+	new_df = new_df.dropna(axis=1, how='all')
+
+	return new_df   
+
 if __name__ == '__main__':
 
-	ph = [1]
+	# Create datacube object
+	dc = datacube.Datacube()
 
-	for pixel in ph:
+	# Create GridWorkflow object so we can work with tiles
+	gw = GridWorkflow(dc.index, product=sref_products[-1])
+
+	# List to store the three datasets (LS5, LS7, LS8)
+	sref_ds = []
 	
-		test_data = pd.read_csv('sample_5.csv')
+	# The key represents which tile we are using
+	key = (5, -28)
 
-		# Sort data by date
-		test_data = test_data.sort_values(by=['datetime'])
+	# Need to fetch the tiles for each product seperately
+	for product in sref_products:
+		
+		gw = GridWorkflow(dc.index, product=product)
 
-		# Only select clear pixels (0 is clear land; 1 is clear water)
-		test_data = test_data[test_data.qa < 2]
+		# Get the list of tiles (one for each time point) for this product
+		tile_list = gw.list_tiles(product=product, cell_index=key)
 
-		# Calculate NDVI: (NIR-red) / (NIR+red)
-		test_data["ndvi"] = (test_data.band_4 - test_data.band_3) / (test_data.band_4 + test_data.band_3)
+		# Load all tiles
+		for tile_index, tile in tile_list.items():
+			dataset = gw.load(tile[0:1, 0:1, 0:10], measurements=['red', 'nir']) # Only need red and NIR to calculate NDVI
 
-		# Set up the plot
-		fig, ax = plt.subplots(figsize=(25, 5))
-		ax.plot(test_data.datetime, test_data.ndvi, 'o', markersize=2)
+			if(dataset.notnull()):
+				sref_ds.append(dataset)
 
-		# Format dates to month/year rather than rata die
-		myFmt = mdates.DateFormatter('%m/%Y')
-		ax.xaxis.set_major_formatter(myFmt)
+	# Close datacube connection to database
+	dc.close()
 
-		pointy = GetChangePoints(ax)
+	# Concatenate the three datasets 
+	sref = xr.concat(sref_ds, dim='time')
 
-		plt.show()		
+	# Change nodata values (0's) to NaN
+	sref = mask_invalid_data(sref)
 
-		xlist = pointy.get_xlist()
+	# We want to process each pixel seperately
+	for i in range(len(sref.x)):
+		for j in range(len(sref.y)):
 
-		print(xlist)
+			# Get the time series of observations for this pixel
+			sref_ts = sref.isel(x=i, y=j)
+	
+			# Transform to pandas dataframe
+			sref_data = transformToDf(sref_ts)
 
-		with open('output_file.csv', 'w') as output_file:
-			file_writer = csv.writer(output_file, delimiter=' ')
+			# Check columns weren't dropped
+			if(sref_data.shape[1] == 3):
 
-			for change in xlist:
-				file_writer.writerow([change[0], change[1]])
+				# Get projected coordinates of this pixel
+				x_val = float(sref_ts.x)
+				y_val = float(sref_ts.y)
+
+				# Set up output file name
+				change_file = str(x_val) + "_" + str(y_val) + ".csv"
+
+				# Calculate NDVI: (NIR-red) / (NIR+red)
+				sref_data["ndvi"] = (sref_data.nir - sref_data.red) / (sref_data.nir + sref_data.red)
+
+				# Set up the plot
+				fig, ax = plt.subplots(figsize=(25, 5))
+				ax.plot(sref_data.datetime, sref_data.ndvi, 'o', markersize=2)
+
+				# Format dates to month/year rather than rata die
+				myFmt = mdates.DateFormatter('%m/%Y')
+				ax.xaxis.set_major_formatter(myFmt)
+
+				pointy = GetChangePoints(ax)
+
+				plt.show()		
+
+				xlist = pointy.get_xlist()
+
+				print(xlist)
+
+				with open(change_file, 'w') as output_file:
+					file_writer = csv.writer(output_file, delimiter=' ')
+
+					for change in xlist:
+						file_writer.writerow([change[0], change[1]])
 
 	
 
